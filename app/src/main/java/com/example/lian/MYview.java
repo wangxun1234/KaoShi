95bf4935d73f2de73af08b95d3c52216546902e2
wangxun1234 <1182406312@qq.com>
2020-03-12 21:40:55 +0800

    练习自定义view

---
+++ app/src/main/java/com/example/lian/MYview.java
@@ -0,0 +1,38 @@
+package com.example.lian;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MYview extends View {
+
+    private String text;
+    private String coler;
+    private int size;
+    Paint paint;
+    public MYview(Context context) {
+        super(context);
+    }
+    public MYview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.test);
+        text = typedArray.getString(R.styleable.test_text);
+        coler = typedArray.getString(R.styleable.test_textcoler);
+        size = typedArray.getInteger(R.styleable.test_textsize, 0);
+        paint = new Paint();
+        paint.setColor(Color.parseColor(coler));
+        paint.setTextSize(size);
+    }
+    public MYview(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawText(text,0,0,paint);
+    }
+}
